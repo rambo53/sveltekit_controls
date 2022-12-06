60fc5d47325210c2038acfc2ab51247ad962d55a
rambo53 <71452106+rambo53@users.noreply.github.com>
2022-12-06 17:16:02 +0100

    Update +page.svelte

--- sveltekit_module_controls/src/routes/controls/+page.svelte
+++ sveltekit_module_controls/src/routes/controls/+page.svelte
@@ -1,30 +1,21 @@
 <script>
-	import { onMount } from 'svelte';
     import { env as public_env} from '$env/dynamic/public';
+    import {getDataFiles} from './controls.js'
 
-    let datas;
-
-    onMount(async () => {
-		const res = await fetch(public_env.PUBLIC_URL_API+'data_file/data_files',{
-                method: 'GET',
-                headers:{
-                    "Accept":"application/json"
-                }
-            }
-        );
-        if(res.ok === true){
-            //datas = await res.json();
-            let data = res.json();
-            console.log(data)
-            //return res.json();
-        }	
-	});
+    let dataFilesPromise = getDataFiles();
 
     let dataFile = "";
     let showSelectConfig = false;
     let showDetailsConfig = true;
+    let configSelect = [];
 
-    const showConfig = async () => {showSelectConfig = !showSelectConfig;}
+    const showConfig = async (file) => {
+        console.log(file);
+        configSelect = file["configs"]
+        console.log(configSelect)
+        showSelectConfig = true;
+    }
+
     const showLstConfig = async () => {showDetailsConfig = !showDetailsConfig;}
 
     const getFileConfig = async () => {
@@ -63,30 +54,46 @@
         <div class="col-5">
             <h3>Fichiers existants :</h3>
 
-            <div class="form-check mb-4 mt-2">
-                <input class="form-check-input" type="radio" bind:group={dataFile} name="data-file" id="1" value="Orcab_bain_V1" on:click={showConfig}>
-                <label class="form-check-label d-flex justify-content-between" for="1">
-                <span>Orcab_bain_V1</span>
-                <span>Jacqueline</span>
-                <span>01/10/2022</span>
-                </label>
-            </div>
+            {#await dataFilesPromise}
+                <h2>Loading....</h2>
+            {:then dataFiles}
+
+                {#each dataFiles as file}
+                    <div class="form-check mb-4 mt-2">
+                        <input class="form-check-input" type="radio" bind:group={dataFile} name="data-file" value="{file.id}" on:click={() => showConfig(file)}>
+                        <label class="form-check-label d-flex justify-content-between" for="1">
+                        <span>{file.file_name}</span>
+                        <span>{file.user_name}</span>
+                        </label>
+                    </div>
+                {/each}
+
+            {:catch error}
+                <h2>{error}</h2>
+            {/await}
+
         </div>
 
         <div class="col-5 {showSelectConfig == false? 'visually-hidden':''}">
             <div class="d-flex justify-content-between">
                 <button class="btn btn-primary mb-4" on:click={showLstConfig}>{showDetailsConfig == true? "Nouvelle configuration" : "Configurations existantes"}</button>
             </div>
+            
+            {#if configSelect.length != 0}
+                
+                <div class="form-check mb-4 {showDetailsConfig == true? '':'visually-hidden'}">
+                    <h3 class="mb-3">Configurations existantes pour ce fichier :</h3>
+                    {#each configSelect as config}
+                        <input class="form-check-input" type="radio" name="config" id="1">
+                        <label class="form-check-label d-flex justify-content-between" for="1">
+                        <span>{config.config_name}</span>
+                        <span>{config.config_user_create}</span>
+                        </label>
+                    {/each}
+                </div>
+                
+            {/if}
 
-            <div class="form-check mb-4 {showDetailsConfig == true? '':'visually-hidden'}">
-                <h3 class="mb-3">Configurations existantes pour ce fichier :</h3>
-                <input class="form-check-input" type="radio" name="config" id="1">
-                <label class="form-check-label d-flex justify-content-between" for="1">
-                <span>Orcab_bain_V1</span>
-                <span>Jacqueline</span>
-                <span>01/10/2022</span>
-                </label>
-            </div>
             <p class="{showDetailsConfig == false? '':'visually-hidden'}">{dataFile} + nouvelle config</p>
             <div class="d-flex justify-content-end">
                 <button class="btn btn-primary">Valider</button>
